14998a7628d139d65626834809b9ad9051f507d3
Karthikeyan <78087029+karthikeyanrao@users.noreply.github.com>
2025-07-09 08:55:48 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtzS2Psq7TS62H_KgSR686qbPw5WVtd7M",
+  apiKey: "Your Api Key",
   authDomain: "returnshield-advitya.firebaseapp.com",
-  projectId: "returnshield-advitya",
+  projectId: "PROJECTID",
   storageBucket: "returnshield-advitya.firebasestorage.app",
   messagingSenderId: "226972892734",
-  appId: "1:226972892734:web:76734601b295c88b041b6f",
+  appId: "APP_ID",
   measurementId: "G-YN4WPL1Q60"
 };
 
